5090c5efb7438d2a6219d6c08052b7702c82671a
atharvakarval <atharva.karval@roxiler.com>
2026-02-05 00:28:14 +0530

    mc

--- streamlit_app/streamlit_app_pwa.py
+++ streamlit_app/streamlit_app_pwa.py
@@ -546,7 +546,13 @@ def render_live_detection():
             video_processor_factory=GuardianDriveDetector,
             media_stream_constraints={"video": True, "audio": False},
             rtc_configuration={
-                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                "iceServers": [
+                    {"urls": ["stun:stun.l.google.com:19302"]},
+                    {"urls": ["stun:stun1.l.google.com:19302"]},
+                    {"urls": ["stun:stun2.l.google.com:19302"]},
+                    {"urls": ["stun:stun3.l.google.com:19302"]},
+                    {"urls": ["stun:stun4.l.google.com:19302"]},
+                ]
             },
             async_processing=True,
         )
